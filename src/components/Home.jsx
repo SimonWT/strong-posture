2764afb20a67a8501fd8adbc351a002dee083675
yaroslav_yudi <yaskih@mail.ru>
2021-12-06 23:20:26 +0700

    add new amplitude event

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,24 @@ import {
 } from "react-router-dom";
 import { Button } from 'ui-neumorphism'
 import PostureGif from '../assets/posture.gif'
+import { sendAmplitudeData } from '../utils/amplitude'
 
-class Home extends React.Component {
-    render () {
-        return <div className="home">
-            <h1>Strong Posture</h1>
-            <img src={PostureGif} alt="" />
-            <div className="home-description">
-                <span>Pomodoro sessions</span>
-                <span>+</span>
-                <span>Reminders to keep posture correct</span>
-            </div>
-            <Link to="/permissions" className="no-underline"> <Button className="main-big-button">Get started</Button></Link>
-        </div>;
+const Home = () => {
+
+    const onGetStartedClick = () => {
+        sendAmplitudeData('get-started-button')
     }
+
+    return (<div className="home">
+        <h1>Strong Posture</h1>
+        <img src={PostureGif} alt="" />
+        <div className="home-description">
+            <span>Pomodoro sessions</span>
+            <span>+</span>
+            <span>Reminders to keep posture correct</span>
+        </div>
+        <Link to="/permissions" onClick={onGetStartedClick} className="no-underline"> <Button className="main-big-button">Get started</Button></Link>
+    </div>)
 }
 
 export default Home
